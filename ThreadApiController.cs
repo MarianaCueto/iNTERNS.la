465d21aecdf7c3d389e292533b674aeaf7a310c6
MarianaCueto <62782165+MarianaCueto@users.noreply.github.com>
2020-03-29 19:13:57 -0700

    Update ThreadApiController.cs

--- ThreadApiController.cs
+++ ThreadApiController.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Sabio.Models;
-using Sabio.Models.Domain;
-using Sabio.Models.Requests;
-using Sabio.Services;
-using Sabio.Services.Interfaces;
-using Sabio.Web.Controllers;
-using Sabio.Web.Models.Responses;
 
-namespace Sabio.Web.Api.Controllers
+namespace .Web.Api.Controllers
 {
     [Route("api/threads")]
     [ApiController]
