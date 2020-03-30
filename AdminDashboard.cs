5994a3247974211131a604229e981fe5a6f95d1b
MarianaCueto <62782165+MarianaCueto@users.noreply.github.com>
2020-03-30 09:14:32 -0700

    Add files via upload

---
+++ AdminDashboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sabio.Models.Domain
+{
+    public class AdminDashboard
+    {
+        public int JobsAppliedForCount { get;set;}
+        public int TotalJobsCount { get;set;}
+        public int EventCount {get;set;}
+        public int OrganizationsCount { get;set;}
+    }
+}
---
+++ TokenAddRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sabio.Models.Requests.Token
+{
+   public class TokenAddRequest
+    {
+        public string Token { get; set; }
+
+        public int UserId { get; set; }
+
+        public int TokenType { get; set; }
+    }
+}
