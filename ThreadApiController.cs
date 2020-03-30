e30f726451aee9624c08dae5dd2700d903864a8b
MarianaCueto <62782165+MarianaCueto@users.noreply.github.com>
2020-03-29 19:13:02 -0700

    Add files via upload

---
+++ ThreadApiController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/threads")]
+    [ApiController]
+    public class ThreadApiController : BaseApiController
+    {
+
+        private IThreadsService _service = null;
+        private IAuthenticationService<int> _authService = null;
+        public ThreadApiController(IThreadsService service
+            , ILogger<ThreadApiController> logger
+            , IAuthenticationService<int> authService) : base(logger)
+        {
+            _service = service;
+            _authService = authService;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<ItemResponse<Thread>> GetById(int id)
+        {
+            int iCode = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Thread thread = _service.Get(id);
+
+
+                if (thread == null)
+                {
+                    iCode = 404;
+                    response = new ErrorResponse("Application Resource not found.");
+
+                }
+                else
+                {
+                    response = new ItemResponse<Thread> { Item = thread };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                iCode = 500;
+                base.Logger.LogError(ex.ToString());
+                response = new ErrorResponse($"Generic Error: {ex.Message}");
+            }
+            return StatusCode(iCode, response);
+        }
+
+        [HttpGet("current")]
+        public ActionResult<ItemResponse<Paged<Thread>>> GetByCurrent(int pageIndex, int pageSize)
+
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                int userId = _authService.GetCurrentUserId();
+                Paged<Thread> page = _service.GetByCurrent(pageIndex, pageSize, userId);
+
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Thread>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+
+
+            return StatusCode(code, response);
+
+        }
+
+        [HttpGet("paginate")]
+        public ActionResult<ItemResponse<Paged<Thread>>> Paginate(int pageIndex, int pageSize)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Paged<Thread> page = _service.Paginate(pageIndex, pageSize);
+
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Thread>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+
+
+            return StatusCode(code, response);
+
+        }
+        [HttpGet("search")]
+        public ActionResult<ItemResponse<Paged<Thread>>> Search(int pageIndex, int pageSize, string search)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Paged<Thread> page = _service.Search(pageIndex, pageSize, search);
+
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Thread>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+
+
+            return StatusCode(code, response);
+
+        }
+
+        [HttpPost]
+        public ActionResult<ItemResponse<int>> Create(ThreadAddRequest model)
+        {
+
+            ObjectResult result = null;
+
+            try
+            {
+                int userId = _authService.GetCurrentUserId();
+                int id = _service.Add(model, userId);
+                ItemResponse<int> response = new ItemResponse<int>();
+                response.Item = id;
+
+                result = Created201(response);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                ErrorResponse response = new ErrorResponse(ex.Message);
+
+                result = StatusCode(500, response);
+            }
+
+            return result;
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<ItemResponse<int>> Update(ThreadUpdateRequest model)
+        {
+            _service.Update(model);
+
+            SuccessResponse response = new SuccessResponse();
+
+            return Ok(response);
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<SuccessResponse> Delete(int id)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                _service.Delete(id);
+
+                response = new SuccessResponse();
+
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+
+            }
+            return StatusCode(code, response);
+        }
+
+    }
+}
