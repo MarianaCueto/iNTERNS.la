1081c13b5bac0a299fe51c5903c15b5db524ae5f
MarianaCueto <62782165+MarianaCueto@users.noreply.github.com>
2020-08-26 14:42:54 -0700

    Add files via upload

---
+++ ContactUs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sabio.Models.Requests.Email
+{
+   public class ContactUs
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }      
+    }
+}
---
+++ EmailBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sabio.Models.Requests.EmailService
+{
+    public class EmailBase
+    {
+
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+    }
+}
