b71ed9bd67e10c62b08c06756106f98ea62bdb37
MarianaCueto <62782165+MarianaCueto@users.noreply.github.com>
2020-09-18 16:18:03 -0700

    Create Email.cs

---
+++ dotnet/Models/Domail/Email.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+   public class Email
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
